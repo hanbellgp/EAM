742bdfd6775dfe0ea3603671cc5ee61e2ef7e5c5
DongTianyu <dtianyu@hanbell.cn>
2017-05-31 21:40:52 +0800

    增加自动产生资产申请功能，UI细节优化
    增加自动产生资产申请功能，实现EFGP资产申请转入EAM资产申请

--- EAM-ejb/src/java/cn/hanbell/eam/ejb/AssetApplyBean.java
+++ EAM-ejb/src/java/cn/hanbell/eam/ejb/AssetApplyBean.java
@@ -7,6 +7,17 @@ package cn.hanbell.eam.ejb;
 
 import cn.hanbell.eam.comm.SuperEJBForEAM;
 import cn.hanbell.eam.entity.AssetApply;
+import cn.hanbell.eam.entity.AssetApplyDetail;
+import cn.hanbell.eam.entity.AssetItem;
+import cn.hanbell.eap.ejb.SystemProgramBean;
+import cn.hanbell.eap.entity.SystemProgram;
+import com.lightshell.comm.SuperEJB;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 
@@ -18,8 +29,49 @@ import javax.ejb.LocalBean;
 @LocalBean
 public class AssetApplyBean extends SuperEJBForEAM<AssetApply> {
 
+    @EJB
+    private SystemProgramBean systemProgramBean;
+
+    @EJB
+    private AssetApplyDetailBean assetApplyDetailBean;
+
+    @EJB
+    private AssetItemBean assetItemBean;
+
     public AssetApplyBean() {
         super(AssetApply.class);
+    }
+
+    public String getFormId(Date day) {
+        SystemProgram sp = systemProgramBean.findBySystemAndAPI("EAM", "assetapply");
+        if (sp == null) {
+            return "";
+        }
+        return super.getFormId(day, sp.getNolead(), sp.getNoformat(), sp.getNoseqlen());
+    }
+
+    public Boolean initAssetApply(AssetApply e, List<AssetApplyDetail> detailList) {
+        if (e == null || detailList == null) {
+            return false;
+        }
+        LinkedHashMap<SuperEJB, List<?>> detailAdded = new LinkedHashMap<>();
+        detailAdded.put(assetApplyDetailBean, detailList);
+        try {
+            String formid = getFormId(e.getFormdate());
+            e.setFormid(formid);
+            for (AssetApplyDetail d : detailList) {
+                d.setPid(formid);
+            }
+            persist(e, detailAdded, null, null);
+            return true;
+        } catch (Exception ex) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex.getMessage());
+            return false;
+        }
+    }
+
+    public AssetItem findByItemno(String itemno) {
+        return assetItemBean.findByItemno(itemno);
     }
 
 }
