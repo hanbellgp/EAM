afc27346160f7167d55b4182206faa551cedfa91
C2079 <871115062@qq.com>
2024-04-17 13:51:18 +0800

    分公司小程序查询语句调整

--- EAM-ejb/src/java/cn/hanbell/eam/ejb/EquipmentSpareStockBean.java
+++ EAM-ejb/src/java/cn/hanbell/eam/ejb/EquipmentSpareStockBean.java
@@ -12,6 +12,7 @@ import cn.hanbell.eam.entity.EquipmentSpareStockResponse;
 import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -32,10 +33,10 @@ public class EquipmentSpareStockBean extends SuperEJBForEAM<EquipmentSpareStock>
     }
 
     //获取库存数量List
-    public List<EquipmentSpareStock> getEquipmentSpareStockList(String sparenum, String sparedesc, String sparemodel,String  company) {
+    public List<EquipmentSpareStock> getEquipmentSpareStockList(String sparenum, String sparedesc, String sparemodel, String company) {
         StringBuilder sb = new StringBuilder();
-        sb.append(" Select T.sparenum,S.sparedesc,S.sparemodel, C.sname,M.mname,sum(T.qty) FROM  equipmentsparestock T  LEFT JOIN equipmentspare S on T.sparenum=S.sparenum AND S.company='"+company+"'");
-        sb.append(" LEFT JOIN equipmentspareclass C ON S.scategory=C.scategory  AND C.company='"+company+"' LEFT JOIN  equipmentsparemid M ON S.scategory=M.scategory AND S.mcategory=M.id Where 1=1  AND T.company='"+company+"'");
+        sb.append(" Select T.sparenum,S.sparedesc,S.sparemodel, C.sname,M.mname,sum(T.qty) FROM  equipmentsparestock T  LEFT JOIN equipmentspare S on T.sparenum=S.sparenum AND S.company='" + company + "'");
+        sb.append(" LEFT JOIN equipmentspareclass C ON S.scategory=C.scategory  AND C.company='" + company + "' LEFT JOIN  equipmentsparemid M ON S.scategory=M.scategory AND S.mcategory=M.id Where 1=1  AND T.company='" + company + "'");
         if (!"".equals(sparenum) && sparenum != null) {
             sb.append(" AND S.sparedesc LIKE ").append("'%").append(sparenum).append("%'");
         }
@@ -56,7 +57,7 @@ public class EquipmentSpareStockBean extends SuperEJBForEAM<EquipmentSpareStock>
     public List<EquipmentSpareStock> getEquipmentSpareStockCheckList(String sarea, String company) {
         StringBuilder sb = new StringBuilder();
         sb.append(" SELECT T.sparenum,S.sparedesc,S.sparemodel, C.sname,M.mname,sum(T.qty),T.sarea,T.slocation FROM  equipmentsparestock T");
-        sb.append(" LEFT JOIN equipmentspare S ON T.sparenum=S.sparenum AND S.company = '"+company+"' LEFT JOIN equipmentspareclass C ON   S.scategory=C.scategory  AND C.company = '"+company+"' LEFT JOIN  equipmentsparemid M ON  S.scategory=M.scategory AND M.company = '"+company+"' AND S.mcategory=M.mcategory");
+        sb.append(" LEFT JOIN equipmentspare S ON T.sparenum=S.sparenum AND S.company = '" + company + "' LEFT JOIN equipmentspareclass C ON   S.scategory=C.scategory  AND C.company = '" + company + "' LEFT JOIN  equipmentsparemid M ON  S.scategory=M.scategory AND M.company = '" + company + "' AND S.mcategory=M.mcategory");
         sb.append(" Where  qty!=0");
         if (!"".equals(sarea) && sarea != null) {
             sb.append(" AND T.sarea='").append(sarea).append("'");
@@ -71,10 +72,10 @@ public class EquipmentSpareStockBean extends SuperEJBForEAM<EquipmentSpareStock>
         return results;
     }
 
-    public List<EquipmentSpareStock> findBySparenum(String sparenum,String company) {
+    public List<EquipmentSpareStock> findBySparenum(String sparenum, String company) {
         Query query = getEntityManager().createNamedQuery("EquipmentSpareStock.findBySparenumAndCompany");
         query.setParameter("sparenum", sparenum);
-         query.setParameter("company", company);
+        query.setParameter("company", company);
         try {
             List results = query.getResultList();
             return results;
@@ -84,19 +85,23 @@ public class EquipmentSpareStockBean extends SuperEJBForEAM<EquipmentSpareStock>
     }
 
     //获取库存数量List
-    public List<EquipmentSpareStockResponse> getEquipmentSpareStockListByNativeQuery(String spareInfo,String company) {
+    public List<EquipmentSpareStockResponse> getEquipmentSpareStockListByNativeQuery(String spareInfo, String company) {
         StringBuilder sb = new StringBuilder();
-        sb.append(" SELECT * FROM equipmentsparestock T LEFT JOIN equipmentspare S ON T.sparenum = S.sparenum WHERE 1 = 1 AND qty > 0  AND T.company='").append(company).append("' ");
+        sb.append(" SELECT * FROM equipmentsparestock T LEFT JOIN equipmentspare S ON T.sparenum = S.sparenum  AND S.company='" + company + "' WHERE 1 = 1 AND qty > 0  AND T.company='").append(company).append("' ");
         if (!"".equals(spareInfo) && spareInfo != null) {
             sb.append(MessageFormat.format(" AND (S.sparedesc LIKE ''%{0}%'' OR T.sparenum LIKE ''%{0}%'' OR S.sparemodel LIKE ''%{0}%'') ", spareInfo));
         }
         //生成SQL
         Query query = getEntityManager().createNativeQuery(sb.toString(), EquipmentSpareStock.class).setMaxResults(50);
         List<EquipmentSpareStock> results = query.getResultList();
-
+           for (EquipmentSpareStock rs : results) {
+            List<EquipmentSpare> eS = this.findBySpare(rs.getSparenum().getSparenum(), rs.getCompany());
+            if (eS.size() > 0) {
+                rs.setSparenum(eS.get(0));
+            }
+        }
         //List按照sparenum分组
         Map<EquipmentSpare, List<EquipmentSpareStock>> groupBySparenumMap = results.stream().collect(Collectors.groupingBy(EquipmentSpareStock::getSparenum));
-
         List<EquipmentSpareStockResponse> resList = new ArrayList<EquipmentSpareStockResponse>();
         groupBySparenumMap.forEach((key, value) -> {
             BigDecimal qtySum = BigDecimal.ZERO;
@@ -176,11 +181,11 @@ public class EquipmentSpareStockBean extends SuperEJBForEAM<EquipmentSpareStock>
         //生成SQL
         Query query = getEntityManager().createNativeQuery(sb.toString());
         List<Object[]> results = query.getResultList();
-        
+
         return results;
     }
-    
-     public List<Object[]> getEquipmentRecord(String formid, String strDate, String endDate) {
+
+    public List<Object[]> getEquipmentRecord(String formid, String strDate, String endDate) {
         StringBuilder sb = new StringBuilder();
         sb.append(" SELECT R.hitchtime,R.hitchreason,R.measure,R.completetime,R.serviceusername FROM equipmentsparerecodedta D LEFT JOIN equipmentsparerecode C");
         sb.append(" ON D.pid=C.formid RIGHT JOIN   equipmentrepair R ON C.relano=R.formid  LEFT JOIN assetcard A ");
@@ -195,10 +200,64 @@ public class EquipmentSpareStockBean extends SuperEJBForEAM<EquipmentSpareStock>
         //生成SQL
         Query query = getEntityManager().createNativeQuery(sb.toString());
         List<Object[]> results = query.getResultList();
-        
+
         return results;
     }
 
- 
+    @Override
+    public List<EquipmentSpareStock> findByFilters(Map<String, Object> filters, int first, int pageSize, Map<String, String> orderBy) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT e FROM ");
+        sb.append(this.className);
+        sb.append(" e WHERE 1=1 ");
+        Map<String, Object> strMap = new LinkedHashMap<>();
+        //给Map排序
+
+        for (Map.Entry<String, Object> entry : filters.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+        }
+
+        if (filters != null) {
+            this.setQueryFilter(sb, filters);
+        }
+        if (orderBy != null && orderBy.size() > 0) {
+            sb.append(" ORDER BY ");
+            for (final Map.Entry<String, String> o : orderBy.entrySet()) {
+                sb.append(" e.").append(o.getKey()).append(" ").append(o.getValue()).append(",");
+            }
+            sb.deleteCharAt(sb.lastIndexOf(","));
+        }
+
+        //生成SQL
+        Query query = getEntityManager().createQuery(sb.toString()).setFirstResult(first).setMaxResults(pageSize);
+
+        //参数赋值
+        if (filters != null) {
+            this.setQueryParam(query, filters);
+
+        }
+        List<EquipmentSpareStock> results = query.getResultList();
+
+        for (EquipmentSpareStock rs : results) {
+            List<EquipmentSpare> eS = this.findBySpare(rs.getSparenum().getSparenum(), rs.getCompany());
+            if (eS.size() > 0) {
+                rs.setSparenum(eS.get(0));
+            }
+        }
+
+        return results;
+    }
+
+    public List<EquipmentSpare> findBySpare(String sparenum, String company) {
+        Query query = getEntityManager().createNamedQuery("EquipmentSpare.findBySparenum");
+        query.setParameter("sparenum", sparenum + "%");
+        query.setParameter("company", company);
+        try {
+            return query.getResultList();
+        } catch (Exception ex) {
+            return null;
+        }
+    }
 
 }
